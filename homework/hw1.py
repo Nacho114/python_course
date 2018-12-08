5966444363df7a3d4d73e86e22449dc8af3b7844
Ignacio Aleman <ial@zurich.ibm.com>
2018-12-08 18:55:23 +0100

    first homework added

---
+++ homework/hw1.py
@@ -0,0 +1,15 @@
+# Problem 0.
+# Make a programa that computes the mean of 3 numbers
+# a, b, c.
+
+# Input  a= 2, b=2 c=2
+# Ouput 2
+
+
+a = 2
+b = 2
+c = 2
+
+mean = (a + b + c) / 3
+
+print(mean)
